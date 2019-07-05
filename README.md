c37d72459200de3347d24c0bd42cdd4d9cf82999
Ken Youens-Clark <kyclark@gmail.com>
2019-07-05 14:03:08 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# b2_web
-B2 Web Interface (back/frontend)
+# B2 Drought Web Interface
+
+* Backend: Node/JS, Mongo
+* Frontend: Elm
+
+# Mongo
+
+Mongo per https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/:
+
+````
+$ brew tap mongodb/brew
+$ brew install mongodb-community@4.0
+$ mongod --config /usr/local/etc/mongod.conf (foreground)
+or
+$ brew services start mongodb-community@4.0 (background)
+$ mongo b2
+````
+
+Export collections on lytic:
+
+````
+$ for coll in experiment specimen; do mongoexport --db b2_project --collection $coll --out b2_${coll}.json; done
+$ tar cvzf b2.tgz b2_*.json
+````
+
+Import locally:
+
+````
+$ for coll in experiment specimen; do mongoimport --db b2_project --collection $coll b2_${coll}.json; done
+````
+
+# Backend
+
+````
+$ cd backend
+$ npm install
+$ DEBUG=backend:* npm start
+````
+
+# Author
+
+Ken Youens-Clark <kyclark@email.arizona.edu>
---
+++ backend/routes/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+const assert = require('assert');
+const Promise = require('promise');
+const config = require('../config.json')
+const MongoClient = require('mongodb').MongoClient;
+
+function mongo() {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(config.mongo.url, (err, db) => {
+          if (err)
+            reject(err)
+          else
+            resolve(db)
+        });
+    });
+}
+
+router.get('/', function (req, res) {
+  mongo().then(function (db) {
+    console.log('here');
+    db.collection('experiment').find().toArray(function (err, results) {
+      if (err) {
+        console.log('error with find');
+      } else {
+        console.log(results);
+      }
+      db.close()
+    });
+    console.log('got collection');
+  });
+
+  res.send('Biosphere2 Drought Project - Hey');
+});
+
+//router.get('/', function (req, res) {
+//  mongo().then(function(db) {
+//    console.log('Connected!');
+//    var coll = db.collection('experiment');
+//    console.log(db);
+//    coll.findOne({}, function (err, doc) {
+//      if (err) {
+//        console.log('error: ' + err)
+//      } else {
+//        console.log(doc);
+//        db.close();
+//        //console.log('experiment_type = ' + x.experiment_type);
+//        //client.close();
+//        //console.log('done');
+//        //res.send('experiment_type = ' + res.experiment_type);
+//        //res.send(doc)
+//      }
+//    })
+//  });
+
+//  res.send('Biosphere2 Drought Project');
+//})
+
+module.exports = router;
