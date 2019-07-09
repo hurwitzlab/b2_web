ce59c88966c1aea1f2a7ede986632d555335a98a
Ken Youens-Clark <kyclark@gmail.com>
2019-07-09 16:15:06 -0700

    basically working, but ugly

--- flask/server.py
+++ flask/server.py
@@ -1,13 +1,40 @@
+"""
+Purpose: B2 Flask App
+Author:  Ken Youens-Clark <kyclark@email.arizona.edu>
+"""
+
 from bson.json_util import dumps
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 url = 'mongodb://localhost:27017/'
 db = 'b2_project'
 client = MongoClient(url)
 mongo_db = client[db]
 
+
 @app.route('/experiments')
-def experiments():
-    return dumps(list(mongo_db['experiment'].find()))
+def experiments_list():
+    """List experiments"""
+
+    # experiment_date_time
+    flds = ('sample_id sample_type run_name experiment_type level '
+            'operator protocol_id').split()
+    proj = {f: 1 for f in flds}
+    return dumps(list(mongo_db['experiment'].find({}, proj)))
+
+
+@app.route('/experiment/<sample_id>')
+def experiment_view(sample_id):
+    """View a single experiment"""
+    # experiment_date_time
+    flds = ('sample_id sample_type run_name experiment_type level '
+            'operator protocol_id').split()
+    proj = {f: 1 for f in flds}
+
+    res = mongo_db['experiment'].find_one({'sample_id': sample_id}, proj)
+
+    return dumps(res or {})
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -7,7 +7,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import HttpBuilder
-import Json.Decode as Decode
+import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Pipeline as Pipeline exposing (custom, optional, required)
 import Json.Encode as Encode exposing (Value)
 import Url
@@ -28,6 +28,9 @@ main =
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , error : Maybe String
+    , experiment : Maybe Experiment
+    , experiments : List Experiment
     }
 
 
@@ -37,19 +40,76 @@ type alias Experiment =
     , run_name : String
     , experiment_type : String
     , level : String
-    , experiment_date_time : String
     , operator : String
     , protocol_id : String
+    , experiment_date_time : String
     }
 
 
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+    | GetExperiment (Result Http.Error Experiment)
+    | SearchSample String
+    | GetExperiments
+    | ListExperiments (Result Http.Error (List Experiment))
 
 
 update msg model =
     case msg of
+        GetExperiments ->
+            ( model, getExperiments )
+
+        ListExperiments result ->
+            let
+                _ =
+                    Debug.log "EXPS = " result
+            in
+            case result of
+                Ok exps ->
+                    ( { model | experiments = exps }, Cmd.none )
+
+                Err _ ->
+                    ( { model | experiments = [] }, Cmd.none )
+
+        GetExperiment result ->
+            let
+                _ =
+                    Debug.log "got result" result
+            in
+            case result of
+                Ok exp ->
+                    ( { model | experiment = Just exp }, Cmd.none )
+
+                Err err ->
+                    let
+                        err_msg =
+                            case err of
+                                Http.Timeout ->
+                                    "Timeout"
+
+                                Http.NetworkError ->
+                                    "Network Error"
+
+                                Http.BadUrl url ->
+                                    "Bad URL: " ++ url
+
+                                Http.BadStatus status ->
+                                    "Bad URL: " ++ String.fromInt status
+
+                                Http.BadBody s ->
+                                    "Bad Response: " ++ s
+                    in
+                    ( { model
+                        | experiment = Nothing
+                        , error = Just err_msg
+                      }
+                    , Cmd.none
+                    )
+
+        SearchSample sample_id ->
+            ( model, getExperiment sample_id )
+
         LinkClicked urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
@@ -68,13 +128,34 @@ init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
     ( { key = key
       , url = url
+      , error = Nothing
+      , experiment = Nothing
+      , experiments = []
       }
     , Cmd.none
     )
 
 
 
---getExperiments : Http.Request (List Experiment)
+-- getExperiment : Int -> Http.Response Experiment
+
+
+getExperiment sample_id =
+    let
+        url =
+            "http://localhost:5000/experiment/" ++ sample_id
+
+        _ =
+            Debug.log "sample_id = " sample_id
+    in
+    HttpBuilder.get url
+        |> HttpBuilder.withExpect
+            (Http.expectJson GetExperiment decoderExperiment)
+        |> HttpBuilder.request
+
+
+
+-- getExperiments : Http.Request (List Experiment)
 
 
 getExperiments =
@@ -83,7 +164,8 @@ getExperiments =
             "http://localhost:5000/experiments"
     in
     HttpBuilder.get url
-        |> HttpBuilder.withExpect (Http.expectJson (Decode.list decoderExperiment))
+        |> HttpBuilder.withExpect
+            (Http.expectJson ListExperiments (Decode.list decoderExperiment))
         |> HttpBuilder.request
 
 
@@ -91,17 +173,79 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "B2"
     , body =
-        [ text "The current URL is: "
-        , b [] [ text (Url.toString model.url) ]
-        , ul []
-            [ viewLink "/home"
-            , viewLink "/profile"
-            , viewLink "/reviews/the-century-of-the-self"
-            , viewLink "/reviews/public-opinion"
-            , viewLink "/reviews/shah-of-shahs"
-            ]
+        [ div [] [ button [ onClick GetExperiments ] [ text "Get Exps" ] ]
+        , div [] [ viewError model.error ]
+        , div [] (viewExperiments model.experiments)
+
+        -- , div [] [ input [ onInput SearchSample ] [] ]
+        --, div [] [ viewExperiment model.experiment ]
         ]
     }
+
+
+viewError error =
+    case error of
+        Just e ->
+            text e
+
+        Nothing ->
+            text ""
+
+
+viewExperiments exps =
+    let
+        numFound =
+            List.length exps
+
+        header =
+            [ tr []
+                [ th [] [ text "sample_id" ]
+                , th [] [ text "operator" ]
+                , th [] [ text "sample_type" ]
+                , th [] [ text "exp_type" ]
+                , th [] [ text "run_name" ]
+                ]
+            ]
+
+        row exp =
+            tr []
+                [ td [] [ text exp.sample_id ]
+                , td [] [ text exp.operator ]
+                , td [] [ text exp.sample_type ]
+                , td [] [ text exp.experiment_type ]
+                , td [] [ text exp.run_name ]
+                ]
+
+        rows =
+            List.map row exps
+
+        tbl =
+            if numFound == 0 then
+                text "No results"
+
+            else
+                table [] (header ++ rows)
+    in
+    [ div [] [ text ("Found " ++ String.fromInt numFound) ]
+    , div [] [ tbl ]
+    ]
+
+
+viewExperiment exp =
+    case exp of
+        Just e ->
+            table []
+                [ tr []
+                    [ td []
+                        [ text "Sample ID" ]
+                    , td
+                        []
+                        [ text e.sample_id ]
+                    ]
+                ]
+
+        Nothing ->
+            text "Nothing to show"
 
 
 viewLink : String -> Html msg
@@ -109,16 +253,17 @@ viewLink path =
     li [] [ a [ href path ] [ text path ] ]
 
 
+decoderExperiment : Decoder Experiment
 decoderExperiment =
     Decode.succeed Experiment
-        |> required "sample_id" Decode.string
-        |> required "sample_type" Decode.string
-        |> required "run_name" Decode.string
-        |> required "experiment_type" Decode.string
-        |> required "level" Decode.string
-        |> required "experiment_date_time" Decode.string
-        |> required "operator" Decode.string
-        |> required "protocol_id" Decode.string
+        |> optional "sample_id" Decode.string ""
+        |> optional "sample_type" Decode.string ""
+        |> optional "run_name" Decode.string ""
+        |> optional "experiment_type" Decode.string ""
+        |> optional "level" Decode.string ""
+        |> optional "operator" Decode.string ""
+        |> optional "protocol_id" Decode.string ""
+        |> optional "experiment_date_time" Decode.string ""
 
 
 
